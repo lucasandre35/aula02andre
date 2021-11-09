129dee6e29b66fd2c078a246c3fc7426eb8de055
lucasandre35 <onek9gamer@gmail.com>
2021-11-09 19:06:16 -0300

    processo de input and output basico

---
+++ logica.js
@@ -0,0 +1,9 @@
+const input = require("readline-sync")
+
+
+const a = "maçã";
+const b = "banana";
+const c = "abacaxi";
+const d = "pera";
+const e = "uva";
+console.log("\n",a,"\n",b,"\n",c,"\n",d,"\n",e)
