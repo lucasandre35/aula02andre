8adabc74bf350cb06cdd12ebcf338cea99031e25
lucasandre35 <onek9gamer@gmail.com>
2021-11-11 21:15:48 -0300

    propriedades for-var

---
+++ aula02/aula03/propriedade.js
@@ -0,0 +1,56 @@
+/*let palavra = "o pai ta on ";
+
+console.log(palavra.length);*/
+
+//------------------
+
+/*var senha = "";
+
+if (senha.length == 8) {
+    console.log("acertou mizeravi");
+} else if (isNaN(senha) || senha == ""){
+    console.log("insira uma senha valida");
+} else {
+    console.log("senha incorreta")
+}*/
+
+//--------------------------
+
+/*let salario = 1000
+while (salario < 5000) {
+    salario += 100
+    
+    console.log("o salario ainda é R$" + salario);}*/
+//------------------------
+
+/*let contador = 0;
+while (contador < 55){
+    console.log('repetição n ' + contador);
+    contador++;
+}*/
+
+//----------------------------
+
+/*const linguagens = ["preço", "pera", "posto"]
+
+let index = 0
+
+do {
+    console.log(linguagens[index]);
+    index++
+} while (index < linguagens.length);*/
+
+//-------------------------
+
+/*for (var i = 0; i < 9; i++){
+    console.log(i);
+}*/
+
+//----
+
+for (var i = 0;; i++){
+    console.log(i);
+    if (i > 300) break;
+}
+
+
